611d6efd96b3ac9a5378c8d10c848d301744afd4
Nathnael <nathnaelzelalem@gmail.com>
2025-03-03 15:16:09 +0300

    update all

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,12 @@ import Users from "./pages/Users";
 import Settings from "./pages/Setting";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp"
-
+import ProtectedRoute from "./ui/ProtectedRoute";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<ProtectedRoute />}>
         <Route path="/" element={<AppLayout />}>
           <Route path="/" element={<Main />}>
             <Route index  element={<Ticket />} />
@@ -18,6 +19,7 @@ function App() {
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
           </Route>
+        </Route>
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
