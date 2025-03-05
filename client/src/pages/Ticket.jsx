8ff26d969472ac226b470aa94c7a3be9327e2681
Nathnael <nathnaelzelalem@gmail.com>
2025-03-05 10:54:57 +0300

    scroll bar style

--- client/src/pages/Ticket.jsx
+++ client/src/pages/Ticket.jsx
@@ -25,7 +25,7 @@ const Ticket = () => {
      
        <h2 className="text-2xl font-bold p-5"><span className="text-pink-700">{`${user.userName}`}</span>'s Tickets </h2>  
       </div>
-      <ul className="p-5">
+      <ul className="p-5 border border-gray-200 h-[65vh] overflow-auto custom-scrollbar">
         {tickets.length > 0 ? (
           <TicketDetail  />
         ) : (
